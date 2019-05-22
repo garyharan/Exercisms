ebccdca145d8883274336e4d25ea45302885fdde
Gary Haran <gary.haran@gmail.com>
2019-05-22 09:52:42 -0400

    rna transcription

---
+++ rna-transcription/rna_transcription.exs
@@ -0,0 +1,25 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna
+    |> Enum.map(&RNATranscription.complement/1)
+    |> Enum.concat
+  end
+
+  def complement(char) do
+    case [char] do
+      'G' -> 'C'
+      'C' -> 'G'
+      'T' -> 'A'
+      'A' -> 'U'
+    end
+  end
+end
