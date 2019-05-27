89494f711dc2e0828aaeac56b6680b3aa8927ac8
Gary Haran <gary.haran@gmail.com>
2019-05-27 14:26:29 -0400

    recursive roman numeral

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -1,12 +1,18 @@
 defmodule Roman do
   @mapping [
-    {59, "LIX"},
-    {40, "XL"},
-    {10, "X"},
-    {9,  "IX"},
-    {5,  "V"},
-    {4,  "IV"},
-    {1,  "I"},
+    {1000, "M"},
+    {900,  "CM"},
+    {500,  "D"},
+    {400,  "CD"},
+    {100,  "C"},
+    {90,   "XC"},
+    {50,   "L"},
+    {40,   "XL"},
+    {10,   "X"},
+    {9,    "IX"},
+    {5,    "V"},
+    {4,    "IV"},
+    {1,    "I"},
   ]
 
   @doc """
@@ -14,10 +20,18 @@ defmodule Roman do
   """
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
-    {0, final_roman_string} = Enum.reduce(@mapping, {number, ""}, fn({arabic, roman}, {number, str}) ->
-      { rem(number, arabic), str <> String.duplicate(roman, div(number, arabic)) }
-    end)
+    romanize(number, "", @mapping)
+  end
 
-    final_roman_string
+  def romanize(number, roman_number, [{arabic_value, roman_value} | _tail] = mapping) when number >= arabic_value do
+    romanize(number - arabic_value, roman_number <> roman_value, mapping)
+  end
+
+  def romanize(number, roman_number, [_head | tail]) do
+    romanize(number, roman_number, tail)
+  end
+
+  def romanize(_number, roman_number, []) do
+    roman_number
   end
 end
